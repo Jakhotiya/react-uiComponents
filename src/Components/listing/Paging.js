168b8a2bef127a80cbf2882877ba9e883f15c0d4
Abhishek Jakhotiya <Jakhotiya@users.noreply.github.com>
2018-01-25 18:40:50 +0530

    begin implementing paging component

--- src/Components/listing/Paging.js
+++ src/Components/listing/Paging.js
@@ -2,10 +2,32 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 
+const Sizes = props=>{
+  return (<h4>Sizes Dropdown</h4>)
+}
+
+
 const Paging = props=>{
   return (
-    <h4>Paging Component</h4>
+    <div className="admin__data-grid-pager-wrap">
+      <Sizes/>
+
+      <div className="admin__data-grid-pager">
+        <button className="action-previous" type="button"  onClick={props.handleClick} disable="isFirst()">
+           Previous Page
+        </button>
+        <input className="admin__control-text" type="number" id="current-page-input"  value={props.currentPage} />
+        <label className="admin__control-support-text" for='current-page-input'>
+           of {props.totalPages}
+        </label>
+        <button className="action-next" type="button" click="next" disable="isLast()" >
+          Next Page
+        </button>  
+      </div>
+    </div>
   );
 }
+
+
 
 export default Paging;
