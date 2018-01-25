cb6729797d2995284ce1d7e7978978c7962b9d5c
Abhishek Jakhotiya <Jakhotiya@users.noreply.github.com>
2018-01-26 03:39:06 +0530

    Got the initial massaction component view  to grid

--- src/Components/listing/MassAction.js
+++ src/Components/listing/MassAction.js
@@ -1,10 +1,45 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const MassAction = props=>{
+const actions=[
+  {label:'Enable',type:'enable'},
+  { label: 'Disable', type: 'disable' },
+  { label: 'Delete', type: 'delete' }
+];
+
+const Action = props=>{
   return (
-    <h4>Mass Action</h4>
+    <li onClick={e=>props.handleAction(props.type)}>
+      <span className="action-menu-item" >{props.label}</span>
+    </li>
   );
+}
+
+class MassAction extends React.Component
+{
+  state={
+    open:false
+  }
+
+  toggleCollapsible =(e)=>{
+    this.setState({open:!this.state.open});
+  } 
+
+  render(){
+    const isActive = this.state.open ? '_active':'';
+    return (
+      <div className={"action-select-wrap "+isActive} onClick={this.toggleCollapsible}>
+        <button className="action-select" title='Select Items'>
+          <span>Actions</span>
+        </button>
+        <div class="action-menu-items">
+        <ul className={"action-menu " + isActive} >
+          {actions.map(action => <Action key={action.type} {...action} />)}
+        </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MassAction;
