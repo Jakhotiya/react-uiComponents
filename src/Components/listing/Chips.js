ec4a3d97bd843ea43fbd6aad285746044f41774a
jakhotiya <jakhotiyaabhishek@gmail.com>
2018-02-02 15:59:03 +0530

    updated package.json dependencies. Project wasn't working after pulling it from codesandbox.io. Fixed props error in Chips.js

--- src/Components/listing/Chips.js
+++ src/Components/listing/Chips.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import PropTypes from 'prop-types';
 
 
-const Chip = ({ label, index, value }) => {
+const Chip = ({ label, index, value,...props }) => {
 
   let text = '';
 
